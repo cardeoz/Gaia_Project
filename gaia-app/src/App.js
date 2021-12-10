1e79685e3d9cf83eac44b8293bb064a1e4cc08de
Catherin08 <85770496+Catherin08@users.noreply.github.com>
2021-12-09 22:13:56 -0500

    Gestión de usuarios
    Interfaz de gestión de usuarios

--- gaia-app/src/App.js
+++ gaia-app/src/App.js
@@ -2,13 +2,15 @@
 import Login from './components/Login';
 import Nav from './components/Nav';
 import { Registro } from './components/Registro';
+import Usuarios from './components/Usuarios';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Registro></Registro>
-      {/* <Login></Login> */}
+      <Login></Login>
+      <Usuarios></Usuarios>
     </div>
   );
 }
